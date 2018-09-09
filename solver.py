7c231146809e9c2d044615eb25829222eb60967a
Omar-Chahbar <ochahba@uwo.ca>
2018-09-08 22:02:00 -0400

    fixed bug found when checking if initial board configuration is valid

--- solver.py
+++ solver.py
@@ -4,7 +4,7 @@ def solvePuzzle(boardString):
     boardArray = stringToArray(boardString)
     valid = checkInitialValidity(boardArray)
     if not valid:
-        print("invalid board!")
+        print("Invalid board!")
         return
     return solvePuzzleRecursively(boardArray,0,0, "forward", boardArray)
 
@@ -29,7 +29,7 @@ def solvePuzzleRecursively(board, row, column, direction, INITIAL_BOARD):
             return solvePuzzleRecursively(board,row,column - 1, direction, INITIAL_BOARD)
  
     board[row][column]+= 1
-    while not checkvalidity(board,row,column):
+    while not checkValidity(board,row,column):
         board[row][column]+= 1
         if board[row][column] == 10:
             board[row][column] = 0
@@ -38,13 +38,16 @@ def solvePuzzleRecursively(board, row, column, direction, INITIAL_BOARD):
     return solvePuzzleRecursively(board,row,column + 1, "forward", INITIAL_BOARD)
 
 def stringToArray(boardString):
-    print("hi")
     row = boardString.split("/")
     board = [[]]
     
     for r in range (0,9):
         board.insert(r, row[r].split(','))
     board.pop(9)
+
+    for r in range (0,9):
+        for c in range (0,9):
+            board[r][c] = int(board[r][c])
     return board
 
 def checkValidity(board, row, column):
@@ -59,7 +62,7 @@ def checkValidity(board, row, column):
     
 def validColumn(board,column,value):
     columnList = []
-    for row in baord:
+    for row in board:
         columnList.append(row[column])
     return columnList.count(value) == 1
 
@@ -69,27 +72,27 @@ def validBox(board,row,column,value):
 
     if row >= 6:
         boxTopRow = 6
-    elif row >= 3
+    elif row >= 3:
         boxTopRow = 3
 
     if column >= 6:
         boxTopColumn = 6
-    elif column >= 3
+    elif column >= 3:
         boxTopColumn = 3
 
     box = []
-    for  for r in range (boxTopRow,boxTopRow + 3):
+    for r in range (boxTopRow,boxTopRow + 3):
         for c in range (boxTopColumn,boxTopColumn + 3):
             box.append(board[r][c])
     
     return box.count(value) == 1
     
 def checkInitialValidity(board):
-    if not InitialRowsValid(board)
+    if not InitialRowsValid(board):
         return False
-    if not InitialColumnsValid(board)
+    if not InitialColumnsValid(board):
         return False
-    if not InitialBoxesValid(board)
+    if not InitialBoxesValid(board):
         return False
     return True
     
@@ -101,7 +104,7 @@ def InitialRowsValid(board):
 
 def InitialColumnsValid(board):
     columnList = []
-    for column in range (0,10)
+    for column in range (0,9):
         for row in board:
             columnList.append(row[column])
         if hasDuplicates(columnList):
@@ -114,7 +117,7 @@ def InitialBoxesValid(board):
     for r in range(0,7,3):
         for c in range(0,7,3):
             box = buildBox(board,r,c)
-            if hasDuplicates(box)
+            if hasDuplicates(box):
                 return False
     return True
     
@@ -128,7 +131,7 @@ def buildBox(board,row,column):
 
 def hasDuplicates(list):
     difference = len(list) - len(set(list))
-    if difference != row.count(0) - 1:
+    if difference != list.count(0) - 1:
         return True
     return False
 
@@ -136,12 +139,23 @@ def init():
 
     BOARD_STRING = ""
 
-    for x in range (1,10):
-        for y in range (1,10):
-            BOARD_STRING += str(y) + ","
-        BOARD_STRING = BOARD_STRING[:-1]
-        BOARD_STRING += "/"
-    BOARD_STRING = BOARD_STRING[:-1]
-    solvePuzzle(BOARD_STRING)
+    # for x in range (1,10):
+    #     for y in range (1,10):
+    #         BOARD_STRING += str(y) + ","
+    #     BOARD_STRING = BOARD_STRING[:-1]
+    #     BOARD_STRING += "/"
+    # BOARD_STRING = BOARD_STRING[:-1]
+    row0 = "0,3,0,0,0,0,6,0,0/"  
+    row1 = "5,0,2,0,0,9,7,0,0/" 
+    row2 = "0,0,0,0,0,4,0,0,0/" 
+    row3 = "7,1,0,0,2,0,0,0,3/" 
+    row4 = "0,0,0,4,0,8,0,0,0/" 
+    row5 = "6,0,0,0,9,0,0,5,7/" 
+    row6 = "0,0,0,8,0,0,0,0,0/" 
+    row7 = "0,0,4,3,0,0,5,0,9/" 
+    row8 = "0,0,7,0,0,0,0,3,0"
+    BOARD_STRING = row0 + row1 + row2 + row3 + row4 + row5 + row6 + row7 + row8
+    solvedPuzzle = solvePuzzle(BOARD_STRING)
+    # print (solvedPuzzle)
 
 init()
