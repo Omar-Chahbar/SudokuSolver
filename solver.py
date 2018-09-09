a076bc8de906e4f8b5e05f6761535a6f4e4e6117
Omar-Chahbar <ochahba@uwo.ca>
2018-09-08 21:35:27 -0400

    implemented checking if board is valid

--- solver.py
+++ solver.py
@@ -1,12 +1,12 @@
 from array import *
 
-def solvePuzzle(boardString, INITIAL_BOARD):
-    boardArray = stringToArray(boardString, INITIAL_BOARD)
-    valid = checkValidity(boardArray)
+def solvePuzzle(boardString):
+    boardArray = stringToArray(boardString)
+    valid = checkInitialValidity(boardArray)
     if not valid:
         print("invalid board!")
         return
-    return solvePuzzleRecursively(boardArray,0,0, "forward", INITIAL_BOARD)
+    return solvePuzzleRecursively(boardArray,0,0, "forward", boardArray)
 
 def solvePuzzleRecursively(board, row, column, direction, INITIAL_BOARD):
     if column > 8:
@@ -29,7 +29,7 @@ def solvePuzzleRecursively(board, row, column, direction, INITIAL_BOARD):
             return solvePuzzleRecursively(board,row,column - 1, direction, INITIAL_BOARD)
  
     board[row][column]+= 1
-    while not checkvalidity(board):
+    while not checkvalidity(board,row,column):
         board[row][column]+= 1
         if board[row][column] == 10:
             board[row][column] = 0
@@ -37,7 +37,7 @@ def solvePuzzleRecursively(board, row, column, direction, INITIAL_BOARD):
 
     return solvePuzzleRecursively(board,row,column + 1, "forward", INITIAL_BOARD)
 
-def stringToArray(boardString, INITIAL_BOARD):
+def stringToArray(boardString):
     print("hi")
     row = boardString.split("/")
     board = [[]]
@@ -45,16 +45,96 @@ def stringToArray(boardString, INITIAL_BOARD):
     for r in range (0,9):
         board.insert(r, row[r].split(','))
     board.pop(9)
-    INITIAL_BOARD = board
-    print(INITIAL_BOARD)
+    return board
 
-def checkValidity(board):
+def checkValidity(board, row, column):
+    value = board[row][column]
+    if board[row].count(value) > 1:
+        return False
+    if not validColumn(board,column,value):
+        return False
+    if not validBox(board,row,column,value):
+        return False
     return True
+    
+def validColumn(board,column,value):
+    columnList = []
+    for row in baord:
+        columnList.append(row[column])
+    return columnList.count(value) == 1
+
+def validBox(board,row,column,value):
+    boxTopRow = 0
+    boxTopColumn = 0
+
+    if row >= 6:
+        boxTopRow = 6
+    elif row >= 3
+        boxTopRow = 3
+
+    if column >= 6:
+        boxTopColumn = 6
+    elif column >= 3
+        boxTopColumn = 3
+
+    box = []
+    for  for r in range (boxTopRow,boxTopRow + 3):
+        for c in range (boxTopColumn,boxTopColumn + 3):
+            box.append(board[r][c])
+    
+    return box.count(value) == 1
+    
+def checkInitialValidity(board):
+    if not InitialRowsValid(board)
+        return False
+    if not InitialColumnsValid(board)
+        return False
+    if not InitialBoxesValid(board)
+        return False
+    return True
+    
+def InitialRowsValid(board):
+    for row in board:
+        if hasDuplicates(row):
+            return False
+    return True  
+
+def InitialColumnsValid(board):
+    columnList = []
+    for column in range (0,10)
+        for row in board:
+            columnList.append(row[column])
+        if hasDuplicates(columnList):
+            return False 
+        columnList.clear()
+    return True
+
+def InitialBoxesValid(board):
+    box = []
+    for r in range(0,7,3):
+        for c in range(0,7,3):
+            box = buildBox(board,r,c)
+            if hasDuplicates(box)
+                return False
+    return True
+    
+def buildBox(board,row,column):
+    box = []
+
+    for r in range(row, row + 3):
+        for c in range(column, column + 3):
+            box.append(board[r][c])
+    return box
+
+def hasDuplicates(list):
+    difference = len(list) - len(set(list))
+    if difference != row.count(0) - 1:
+        return True
+    return False
 
 def init():
 
     BOARD_STRING = ""
-    INITIAL_BOARD = [[]]
 
     for x in range (1,10):
         for y in range (1,10):
@@ -62,6 +142,6 @@ def init():
         BOARD_STRING = BOARD_STRING[:-1]
         BOARD_STRING += "/"
     BOARD_STRING = BOARD_STRING[:-1]
-    solvePuzzle(BOARD_STRING, INITIAL_BOARD)
+    solvePuzzle(BOARD_STRING)
 
 init()
